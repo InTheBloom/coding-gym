86cf10037b466997ed3437ed29661cc93b480997
InTheBloom@wsl <nato.rider.smm2@gmail.com>
2025-06-24 03:36:43 +0900

    ジャッジが機能するようにした

--- routes/problem/submit.js
+++ routes/problem/submit.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 const router = express.Router({ mergeParams: true });
 
 const { db } = require("../../db");
+const { judgeSubmission } = require("../../utils/judge");
 
 router.post('/', async function(req, res, next) {
     const uploadedFile = req.files?.answer_file;
@@ -11,36 +12,34 @@ router.post('/', async function(req, res, next) {
         return res.redirect(`/problem/${req.params.problem_number}`);
     }
 
-    let submittedAnswer;
-    try {
-        submittedAnswer = await fs.readFile(uploadedFile.path, 'utf-8');
-    }
-    catch (err) {
-        req.session.errorMessage = "ファイルの読み込みに失敗しました。";
-        return res.redirect(`/problem/${req.params.problem_number}`);
-    }
-    const submittedCode = typeof req.body.code === "string" ? req.body.code : "";
-
-    const MAX_CODE_BYTE = 100000;
-    if (MAX_CODE_BYTE < Buffer.byteLength(submittedCode, 'utf-8')) {
-        req.session.errorMessage = "コードは100KB以下である必要があります。";
-        return res.redirect(`/problem/${req.params.problem_number}`);
-    }
-
-    // TODO: judgeして結果を返す。
-    const isCorrect = true;
-    const feedback = "Accepted.";
-
     // problem_number -> problem_idの変換
-    const problem = db.prepare("SELECT id FROM problems WHERE problem_number = ?").get(req.params.problem_number);
+    const problem = db.prepare("SELECT id, testcase_output_file FROM problems WHERE problem_number = ?").get(req.params.problem_number);
     if (!problem) {
         req.session.errorMessage = "問題が存在しません。";
         return res.redirect("/problem");
     }
 
-    // dbへのINSERT
+    const MAX_CODE_BYTE = 100000;
+    const submittedCode = typeof req.body.code === "string" ? req.body.code : "";
+    if (MAX_CODE_BYTE < Buffer.byteLength(submittedCode, 'utf-8')) {
+        req.session.errorMessage = "コードは100KB以下である必要があります。";
+        return res.redirect(`/problem/${req.params.problem_number}`);
+    }
+
+    let isCorrect, feedback;
     try {
-        db.prepare(`
+        ({ isCorrect, feedback } = await judgeSubmission(problem.testcase_output_file, uploadedFile.path));
+    }
+    catch (err) {
+        req.session.errorMessage = "ファイルの読み込みに失敗しました。";
+        console.error(err);
+        return res.redirect(`/problem/${req.params.problem_number}`);
+    }
+
+    // dbへのINSERT
+    let info;
+    try {
+        info = db.prepare(`
             INSERT INTO submissions (user_id, problem_id, code, is_correct, feedback)
             VALUES (?, ?, ?, ?, ?)
         `).run(req.session.userId, problem.id, submittedCode, isCorrect ? 1 : 0, feedback);
@@ -51,7 +50,7 @@ router.post('/', async function(req, res, next) {
         return res.redirect(`/problem/${req.params.problem_number}`);
     }
 
-    res.redirect(`/problem/${req.params.problem_number}/submissions`);
+    res.redirect(`/problem/${req.params.problem_number}/submissions/${info.lastInsertRowid}`);
 });
 
 module.exports = router;
--- utils/judge.js
+++ utils/judge.js
@@ -1,22 +1,45 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+function normalizeNewLines(text) {
+    return text.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+}
+
 async function judgeSubmission(answerPath, expectedPath) {
     const [userOutput, expectedOutput] = await Promise.all([
         fs.readFile(answerPath, 'utf-8'),
-        fs.readFile(expectedPath)
+        fs.readFile(expectedPath, 'utf-8')
     ]);
 
-    const userOutputByLine = userOutput.trimEnd().split("\n");
-    const expectedOutputByLine = expectedOutput.trimEnd().split("\n");
+    const userOutputByLine = normalizeNewLines(userOutput).trimEnd().split("\n");
+    const expectedOutputByLine = normalizeNewLines(expectedOutput).trimEnd().split("\n");
 
-    const maxRow = Math.max(userOutputByLine.length(), expectedOutputByLine.length());
+    const maxRow = Math.max(userOutputByLine.length, expectedOutputByLine.length);
 
     for (let i = 0; i < maxRow; i++) {
-        // 行の不一致
-        return { is_correct: false, feedback: 'Wrong Answer.' };
+        if (userOutputByLine.length <= i) {
+            return {
+                isCorrect: false,
+                feedback: `${i + 1}行目以降の出力が存在しません。`
+            }
+        }
+
+        const ul = userOutputByLine[i] ?? '';
+        const el = expectedOutputByLine[i] ?? '';
+
+        for (let j = 0; j < Math.max(ul.length, el.length); j++) {
+            const u = ul[j];
+            const e = el[j];
+            if (u !== e) {
+                return {
+                    isCorrect: false,
+                    feedback: `${i + 1}行目、${j + 1}文字目が一致しません。`
+                }
+            }
+        }
     }
 
-    return { is_correct: true, feedback: 'Accepted.' };
+    return { isCorrect: true, feedback: 'Accepted.' };
 }
 
+module.exports = { judgeSubmission };
